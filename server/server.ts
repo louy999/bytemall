972ab41edd9ab0a830621c30ecb6f1bd34c23ade
louy999 <louyhany999@gmail.com>
2023-12-05 13:46:55 +0200

    update files

--- server/server.ts
+++ server/server.ts
@@ -41,16 +41,6 @@ app.use(
 	})
 )
 
-// app.use((req, res, next) => {
-// 	res.header(
-// 		'Access-Control-Allow-Origin',
-// 		'https://bytemall-louy999.vercel.app'
-// 	)
-// 	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-// 	res.header('Access-Control-Allow-Headers', 'Content-Type')
-// 	next()
-// })
-
 //security and consol req
 app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}))
 
